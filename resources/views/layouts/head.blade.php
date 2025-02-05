e3f86b192274b0e208f17df47aa3ea0d8db36f96
pedrolazzaroni <phlazzaroni@gmail.com>
2025-02-05 15:25:01 -0300

    Adicionando cdn tailwind

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     @include('layouts.styles')
 </head>
 <body class="bg-gray-100">
