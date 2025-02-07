a8da9fe8f591cb2cc72c618ea386650759fcb7a8
pedrolazzaroni <phlazzaroni@gmail.com>
2025-02-07 14:43:50 -0300

    Compilando o css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-q_0pR2Fx.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     {{-- @vite('resources/css/app.css') --}}
     {{-- <script src="https://unpkg.com/@tailwindcss/browser@4"></script> --}}
     @include('layouts.styles')
