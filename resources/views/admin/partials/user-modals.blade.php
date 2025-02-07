788c9ef88c388dceca9bb5b1552a3d166a9e03fe
pedrolazzaroni <phlazzaroni@gmail.com>
2025-02-07 14:40:12 -0300

    Finalizando detalhes

--- resources/views/admin/partials/user-modals.blade.php
+++ resources/views/admin/partials/user-modals.blade.php
@@ -56,7 +56,7 @@
 </div>
 
 <!-- Modal de Edição de Usuário -->
-<div id="editModal" class="fixed inset-0 bg-black bg-opacity-50 z-50 hidden">
+<div id="editModal" class="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 hidden  transition-opacity duration-300">
     <div class="flex items-center justify-center min-h-screen px-4">
         <div class="bg-white rounded-lg shadow-xl max-w-md w-full p-6">
             <div class="flex justify-between items-center mb-4">
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{ asset('build/assets/app-q_0pR2Fx.css')}}">
     {{-- @vite('resources/css/app.css') --}}
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    {{-- <script src="https://unpkg.com/@tailwindcss/browser@4"></script> --}}
     @include('layouts.styles')
 </head>
 <body class="bg-gray-100">
