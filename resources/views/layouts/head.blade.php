926bcf93993c65018170185d402c246dca7bf1a0
pedrolazzaroni <phlazzaroni@gmail.com>
2025-02-07 15:12:45 -0300

    Atualizando Favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     {{-- @vite('resources/css/app.css') --}}
     {{-- <script src="https://unpkg.com/@tailwindcss/browser@4"></script> --}}
     @include('layouts.styles')
