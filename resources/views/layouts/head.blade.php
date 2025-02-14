670adb075e918de71da5d30fbcf94d0e5fdba6f2
pedrolazzaroni <phlazzaroni@gmail.com>
2025-02-14 14:21:29 -0300

    Salvando

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css')}}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
     {{-- <script src="https://unpkg.com/@tailwindcss/browser@4"></script> --}}
     @include('layouts.styles')
 </head>
